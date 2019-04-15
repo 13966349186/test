2db998cd767f91f111ced89d5a48071421db640e
1846721817@qq.com <xia1846721817>
2019-04-16 00:01:55 +0800

    c1

---
+++ src/Abstarfish/Starfish/Tools.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Abstarfish\Starfish;
+
+
+class Tools
+{
+    public function __construct(){
+
+    }
+    public static function Hello(){
+        echo 'hello world';
+    }
+}
