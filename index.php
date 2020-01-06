40a1ab0ca4bca2affcac24de50fc277edf8ddc1d
yu <1846721817@qq.com>
2020-01-06 14:31:18 +0800

    new data

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * link: http://www.zjhejiang.com/
+ * copyright: Copyright (c) 2020 浙江禾匠信息科技有限公司
+ * author: xay
+ */
+require_once "./vendor/autoload.php";
+
+use src\Tools;
+
+$a = new Tools();
+echo $a::Hello();
---
+++ src/Tools.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace src;
+
+class Tools
+{
+    public function __construct()
+    {
+
+    }
+
+    public static function Hello()
+    {
+        echo 'Hello World';
+    }
+}
